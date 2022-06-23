1d4d00ea444c59b5855b25cd41e4e609b9633935
Gustek <gustek@riseup.net>
2022-06-23 15:59:18 +0200

    Add Stumwpm keybinding

--- stumpwm/keys.lisp
+++ stumpwm/keys.lisp
@@ -9,5 +9,7 @@
 (bind-key "C-r" "loadrc")
 (bind-key "C-I" "modules-init")
 
+(bind-key "M-f" "exec firefox")
+
 (run-shell-command "setxkbmap -option caps:escape")
 (run-shell-command "xmodmap -e 'keysym Shift_R = Multi_key'")
