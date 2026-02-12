6361d4e79904d124fa661ac7a87c5ef1a9c3f188
Gustek <gustek@riseup.net>
2026-02-12 15:48:55 +0100

    Customise dwm keybindings for workman

--- dwm/config.h
+++ dwm/config.h
@@ -79,12 +79,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_e,      focusstack,     {.i = +1 } }, /* XK_j */
+	{ MODKEY,                       XK_o,      focusstack,     {.i = -1 } }, /* XK_k */
+	{ MODKEY,                       XK_u,      incnmaster,     {.i = +1 } }, /* XK_i */
+	{ MODKEY,                       XK_r,      incnmaster,     {.i = -1 } }, /* XK_d */
+	{ MODKEY,                       XK_n,      setmfact,       {.f = -0.05} }, /* XK_h */
+	{ MODKEY,                       XK_i,      setmfact,       {.f = +0.05} }, /* XK_l */
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
